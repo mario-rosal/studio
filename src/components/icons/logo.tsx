304948dfa5a5a5e9f0b8519b5050b9e8de096a70
Mario Rosal <mario.rosal@mytaskpanel.com>
2025-07-11 13:00:18 +0000

    no, mejor por un ícono como el que tenías al principio, este logo lo has

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -5,38 +5,12 @@ export function Logo(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 100 100"
-      fill="none"
+      viewBox="0 0 24 24"
+      fill="currentColor"
       {...props}
     >
-      <g>
-        {/* Central Hexagon - using multiple paths for different face colors */}
-        <path d="M50 25 L71.65 37.5 L50 50 L28.35 37.5 Z" fill="#2C3E50" />
-        <path d="M28.35 37.5 L50 50 L50 75 L28.35 62.5 Z" fill="#34495E" />
-        <path d="M71.65 37.5 L71.65 62.5 L50 75 L50 50 Z" fill="#5D7A99" />
-
-        {/* Checkmark */}
-        <path
-          d="M45 53 l-7 -7 l3 -3 l4 4 l8 -8 l3 3 z"
-          fill="#34495E"
-          fillOpacity="0.7"
-        />
-
-        {/* Arms and Circles */}
-        <g stroke="#5D7A99" strokeWidth="4" strokeLinecap="round">
-          {/* Top-left arm */}
-          <path d="M28.35 37.5 l-15 -8.66" />
-          <circle cx="10.35" cy="27.84" r="5" fill="#5D7A99" />
-          
-          {/* Top-right arm */}
-          <path d="M71.65 37.5 l15 -8.66" />
-          <circle cx="89.65" cy="27.84" r="5" fill="#5D7A99" />
-
-          {/* Bottom arm */}
-          <path d="M50 75 v20" />
-          <circle cx="50" cy="98" r="5" fill="#5D7A99" />
-        </g>
-      </g>
+      <path d="M13.786 4.467l-5.62 5.62-1.31-1.31a.75.75 0 00-1.06 1.06l1.839 1.84a.75.75 0 001.06 0l6.15-6.15a.75.75 0 10-1.06-1.06z" />
+      <path fillRule="evenodd" d="M12 21a9 9 0 100-18 9 9 0 000 18zm0 1.5a10.5 10.5 0 100-21 10.5 10.5 0 000 21z" clipRule="evenodd" />
     </svg>
   );
 }
